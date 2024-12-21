61008a63613ca1818512ca7d8114b95770fb2f90
Brian Pepple <bdpepple@gmail.com>
2024-12-21 15:47:33 -0500

    Refactor `fix_issue_story` management command

--- comicsdb/management/commands/fix_issue_story.py
+++ comicsdb/management/commands/fix_issue_story.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from django.core.management.base import BaseCommand
 
 from comicsdb.models.issue import Issue
@@ -8,12 +6,8 @@ from comicsdb.models.issue import Issue
 class Command(BaseCommand):
     help = "Fix bad issue story data."
 
-    def handle(self, *args: Any, **options: Any) -> str | None:
-        qs = Issue.objects.filter(name=None)
-        if qs.count() > 0:
-            for i in qs:
-                i.name = []
-            fix_count = Issue.objects.bulk_update(qs, ["name"])
+    def handle(self, *args, **options):
+        if fix_count := Issue.objects.filter(name=None).update(name=[]):
             self.stdout.write(self.style.SUCCESS(f"Fixed {fix_count} issues."))
         else:
             self.stdout.write(self.style.WARNING("Nothing to fix."))
