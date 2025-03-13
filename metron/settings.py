ba1f1dba085e78c6926113ddaaf6c47f67cd8bef
Brian Pepple <bdpepple@gmail.com>
2025-03-13 16:15:23 -0400

    Change API default API permission to `DjangoModelPermissions` (#346)
    * Change API default API permission to `DjangoModelPermissions`
    
    One side effect of this change is that regular users will not  Issue form's autocomplete light widget creator add functionality.
    
    Could potentially get around that by using custom permissions, but given just how *many* creators are accidentally added I'm sure it's worth the time.
    
    * Update staff fixture to include model permissions for POST / PATCH test

--- metron/settings.py
+++ metron/settings.py
@@ -163,7 +163,7 @@ THUMBNAIL_DEBUG = DEBUG
 
 # REST settings
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.DjangoModelPermissions",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
