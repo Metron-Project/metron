f393a8b5486ada29d5fecddfd6eca2f445937f18
Brian Pepple <bdpepple@gmail.com>
2025-10-21 08:45:39 -0400

    Revert "Use HTMX to provide live statistics (#385)"
    This reverts commit e8b2e37a5ca29dc5ab3b38e1d8387c445658b61c.

--- comicsdb/urls/home.py
+++ comicsdb/urls/home.py
@@ -1,13 +1,11 @@
 from django.urls import path
 
 from comicsdb.views.home import HomePageView, recently_edited_issues
-from comicsdb.views.statistics import statistics, statistics_charts, statistics_totals
+from comicsdb.views.statistics import statistics
 
 app_name = ""
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("statistics/", statistics, name="statistics"),
-    path("statistics/totals/", statistics_totals, name="statistics-totals"),
-    path("statistics/charts/", statistics_charts, name="statistics-charts"),
     path("recently-edited/", recently_edited_issues, name="recently-edited"),
 ]
