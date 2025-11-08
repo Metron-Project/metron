5670a7ed79bde5e98fca97632180dd4dcb755135
Brian Pepple <bdpepple@gmail.com>
2025-11-08 16:11:52 -0500

    Modify ArcDetailView to lazy load

--- comicsdb/views/arc.py
+++ comicsdb/views/arc.py
@@ -1,15 +1,14 @@
 import logging
 
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.db.models import Prefetch
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from comicsdb.forms.arc import ArcForm
 from comicsdb.models.arc import Arc
-from comicsdb.models.issue import Issue
 from comicsdb.views.constants import PAGINATE_BY
 from comicsdb.views.history import HistoryListView
 from comicsdb.views.mixins import (
@@ -45,14 +44,24 @@ class ArcIssueList(ListView):
 
 class ArcDetail(NavigationMixin, DetailView):
     model = Arc
-    queryset = Arc.objects.select_related("edited_by").prefetch_related(
-        Prefetch(
-            "issues",
-            queryset=Issue.objects.order_by(
+    queryset = Arc.objects.select_related("edited_by")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        arc = context["object"]
+
+        # Get issue count and paginate
+        issue_count = arc.issues.count()
+        context["issue_count"] = issue_count
+
+        # Only load first 30 issues
+        if issue_count > 0:
+            issues_qs = arc.issues.order_by(
                 "cover_date", "store_date", "series__sort_name", "number"
-            ).select_related("series", "series__series_type"),
-        )
-    )
+            ).select_related("series", "series__series_type")
+            context["issues"] = issues_qs[:30]
+
+        return context
 
 
 class ArcDetailRedirect(SlugRedirectView):
@@ -87,3 +96,30 @@ class ArcDelete(PermissionRequiredMixin, DeleteView):
 
 class ArcHistory(HistoryListView):
     model = Arc
+
+
+class ArcIssuesLoadMore(View):
+    """HTMX endpoint for lazy loading more arc issues."""
+
+    def get(self, request, slug):
+        arc = get_object_or_404(Arc, slug=slug)
+        offset = int(request.GET.get("offset", 0))
+        limit = 30  # Load 30 items at a time
+
+        # Same query as in ArcDetail.get_context_data
+        issues_qs = arc.issues.order_by(
+            "cover_date", "store_date", "series__sort_name", "number"
+        ).select_related("series", "series__series_type")
+
+        total_count = issues_qs.count()
+        paginated_issues = issues_qs[offset : offset + limit]
+
+        has_more = total_count > offset + limit
+
+        context = {
+            "issues": paginated_issues,
+            "has_more": has_more,
+            "next_offset": offset + limit,
+            "arc_slug": slug,
+        }
+        return render(request, "comicsdb/partials/arc_issue_items.html", context)
