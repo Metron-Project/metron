2e86363b4d3e8c0f00860a93213c6f68ef3b8704
Brian Pepple <bdpepple@gmail.com>
2025-11-19 08:38:26 -0500

    Update help text for `reprints` field

--- comicsdb/forms/issue.py
+++ comicsdb/forms/issue.py
@@ -99,9 +99,7 @@ class IssueForm(ModelForm):
             "name": "Separate multiple story titles by a semicolon",
             "title": "Only used with Collected Editions like a Trade Paperback.",
             "price": "In United States currency",
-            "reprints": (
-                "Add any issues that are reprinted. Do not add a '#' in front of any issue number."
-            ),
+            "reprints": "Search for and select any issues that are reprinted in this issue.",
             "foc_date": "This date should be earlier than the store date",
         }
         labels = {
