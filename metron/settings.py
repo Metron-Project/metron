9430af780c3f712d182606678eb69eea54c8b97f
Brian Pepple <bdpepple@gmail.com>
2026-03-04 14:06:46 -0500

    Reduce the API Rate Limit (#470)
    The new values are:
    
    - 20/minute
    - 5,000/day

--- metron/settings.py
+++ metron/settings.py
@@ -195,7 +195,7 @@ REST_FRAMEWORK = {
         "api.throttle.BurstRateThrottle",
         "api.throttle.SustainedRateThrottle",
     ],
-    "DEFAULT_THROTTLE_RATES": {"burst": "35/minute", "sustained": "10000/day"},
+    "DEFAULT_THROTTLE_RATES": {"burst": "20/minute", "sustained": "5000/day"},
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,
