9194178cd045fff425666edc8f9c60543d0a5d2c
Brian Pepple <bdpepple@gmail.com>
2026-06-09 11:37:20 -0400

    Remove "Recently Edited Issue" section from homepage

--- comicsdb/views/home.py
+++ comicsdb/views/home.py
@@ -1,32 +1,5 @@
-from datetime import datetime
-
-from django.core.cache import cache
 from django.views.generic.base import TemplateView
-
-from comicsdb.models import Issue
-
-# Cache time to live is 30 minutes.
-CACHE_TTL = 60 * 30
 
 
 class HomePageView(TemplateView):
     template_name = "comicsdb/home.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # time when cached
-        update_time = cache.get("home_updated")
-        if not update_time:
-            update_time = datetime.now()
-            cache.set("home_updated", update_time, CACHE_TTL)
-        context["updated"] = update_time
-
-        # recently edited issues
-        recently_edited = cache.get("recently_edited")
-        if not recently_edited:
-            recently_edited = Issue.objects.select_related("series").order_by("-modified")[:12]
-            cache.set("recently_edited", recently_edited, CACHE_TTL)
-
-        context["recently_edited"] = recently_edited
-
-        return context
